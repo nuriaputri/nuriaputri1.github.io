f7fb9a7f1d6a02247b4b38ef3f2e5b029df22790
Nuria Putri <61985028+nuriaputri@users.noreply.github.com>
2021-07-22 13:36:23 +0700

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '52bea1e662c117353bfcc64151625f36f42a3b06',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '52bea1e662c117353bfcc64151625f36f42a3b06',
+            'dev_requirement' => false,
+        ),
+    ),
+);
